d353ecc49048b59e97b97661f1d42e8653a76dd5
jtorbett23 <josh_torbett@hotmail.co.uk>
2021-01-29 11:48:48 +0000

    Move images to public folder

--- src/Containers/Info/Info.jsx
+++ src/Containers/Info/Info.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default function Info(props) {
+    const imagesPath = `${process.env.PUBLIC_URL}/images/`
+
     return (
         <div>
             Information
             { props.info &&
             <>
-            <img src={props.imagesPath + props.info.profile_image.name + '.png'} alt={props.info.profile_image.alt}/>
+            <img src={`${imagesPath}${props.info.profile_image.name}`} alt={props.info.profile_image.alt}/>
             <p id="name">Name: {props.info.name}</p>
             <p id="location">Location: {props.info.location}</p>
             Github: <a id="github" href={props.info.github_url} target="_blank" rel="noreferrer">GITHUB</a> <br/>
--- src/Containers/Info/Info.spec.jsx
+++ src/Containers/Info/Info.spec.jsx
@@ -11,12 +11,12 @@ describe('Intro Container Tests', ()=>{
 
     it('should render the correct content', () =>{
 
-        const testImagesPath = "../test/images/"
+        const testImagesPath = `${process.env.PUBLIC_URL}/images/`
         const testInformation = {
             name: 'Testman',
             location: 'Test land',
             profile_image: {
-                name: 'testman', 
+                name: 'testman.png', 
                 alt: 'Picure of testman'
             },
             github_url: 'githubby/testman',
@@ -29,7 +29,7 @@ describe('Intro Container Tests', ()=>{
         exp(wrapper.find('#location')).to.have.text("Location: " + testInformation.location)
         exp(wrapper.find('#github')).to.have.attr('href', testInformation.github_url)
         exp(wrapper.find('#linkedin')).to.have.attr('href', testInformation.linkedin_url)
-        const fullTestImagePath = testImagesPath + testInformation.profile_image.name + '.png'
+        const fullTestImagePath = testImagesPath + testInformation.profile_image.name
         exp(wrapper.find('img')).to.have.attr('src', fullTestImagePath)
         exp(wrapper.find('img')).to.have.attr('alt', testInformation.profile_image.alt)
 
