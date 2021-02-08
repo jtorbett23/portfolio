a75e5c6c5e9f7e920ce60497a58cc542b9cae55c
jtorbett23 <josh_torbett@hotmail.co.uk>
2021-02-08 12:56:44 +0000

    Add icon links for github, linkedin, email

--- src/Containers/Info/Info.jsx
+++ src/Containers/Info/Info.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Info.css'
 
 export default function Info(props) {
     const imagesPath = `${process.env.PUBLIC_URL}/images/`
@@ -7,11 +8,20 @@ export default function Info(props) {
         <div>
             { props.info &&
             <>
-            <img src={`${imagesPath}${props.info.profile_image.name}`} alt={props.info.profile_image.alt}/>
-            <p id="name">Name: {props.info.name}</p>
-            <p id="location">Location: {props.info.location}</p>
-            Github: <a id="github" href={props.info.github_url} target="_blank" rel="noreferrer">GITHUB</a> <br/>
-            LinkedIn: < a id="linkedin" href={props.info.linkedin_url} target="_blank" rel="noreferrer">LINKEDIN</a>
+            <img id="profile-image" src={`${imagesPath}${props.info.profile_image.name}`} alt={props.info.profile_image.alt}/>
+            <p id="name">{props.info.name}</p>
+            <p id="location">{props.info.location}</p>
+            <div className="info-links-container">
+            <a id="github" href={props.info.github_url} target="_blank" rel="noreferrer"> 
+                <img className="image-link" src={`${imagesPath}github-logo.png`} alt="Github Logo"/>
+            </a> 
+            <a id="linkedin" href={props.info.linkedin_url} target="_blank" rel="noreferrer">
+                <img className="image-link" src={`${imagesPath}linkedin-logo.png`} alt="Linkedin Logo"/>
+            </a>
+            <a id="email" href={props.info.email_url} target="_blank" rel="noreferrer">
+                <img className="image-link" src={`${imagesPath}email-logo.png`} alt="Email Logo"/>
+            </a>
+            </div>
             </>
             }
         </div>
--- src/Containers/Info/Info.spec.jsx
+++ src/Containers/Info/Info.spec.jsx
@@ -20,18 +20,25 @@ describe('Intro Container Tests', ()=>{
                 alt: 'Picure of testman'
             },
             github_url: 'githubby/testman',
-            linkedin_url: 'linkedin/testman'
-
+            linkedin_url: 'linkedin/testman',
+            email_url: 'mailto:testman@email.com'
         }
         wrapper = shallow(<Info info={testInformation} imagesPath={testImagesPath}/>)
 
-        exp(wrapper.find('#name')).to.have.text("Name: " + testInformation.name)
-        exp(wrapper.find('#location')).to.have.text("Location: " + testInformation.location)
+        exp(wrapper.find('#name')).to.have.text(testInformation.name)
+        exp(wrapper.find('#location')).to.have.text(testInformation.location)
         exp(wrapper.find('#github')).to.have.attr('href', testInformation.github_url)
+        exp(wrapper.find('#github').find('img')).to.have.attr('src', `${testImagesPath}github-logo.png`)
+        exp(wrapper.find('#github').find('img')).to.have.attr('alt', 'Github Logo')
         exp(wrapper.find('#linkedin')).to.have.attr('href', testInformation.linkedin_url)
+        exp(wrapper.find('#linkedin').find('img')).to.have.attr('src', `${testImagesPath}linkedin-logo.png`)
+        exp(wrapper.find('#linkedin').find('img')).to.have.attr('alt', 'Linkedin Logo')
+        exp(wrapper.find('#email')).to.have.attr('href', testInformation.email_url)
+        exp(wrapper.find('#email').find('img')).to.have.attr('src', `${testImagesPath}email-logo.png`)
+        exp(wrapper.find('#email').find('img')).to.have.attr('alt', 'Email Logo')
         const fullTestImagePath = testImagesPath + testInformation.profile_image.name
-        exp(wrapper.find('img')).to.have.attr('src', fullTestImagePath)
-        exp(wrapper.find('img')).to.have.attr('alt', testInformation.profile_image.alt)
+        exp(wrapper.find('#profile-image')).to.have.attr('src', fullTestImagePath)
+        exp(wrapper.find('#profile-image')).to.have.attr('alt', testInformation.profile_image.alt)
 
 
 
