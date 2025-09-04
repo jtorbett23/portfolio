04ffb5315adb2f0d27b173a5a8f9dbbb0f957dcf
jtorbett23 <josh.torbett@outlook.com>
2025-09-04 11:33:03 +0100

    replace buttons with clickable areas

--- src/components/book/book.tsx
+++ src/components/book/book.tsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react"
-import {table, sleeve, button, bookContainer, page} from "../../styles/book.module.css"
+import {table, sleeve, bookContainer, page, leftPageTrigger, rightPageTrigger} from "../../styles/book.module.css"
 
 const Book = () => {
   const [pageNumber, setPageNumber] = useState(1)
   const minPage = 1
   const maxPage = 3
+  const decreasePageNumber = () => {
+   if (pageNumber > minPage)
+          setPageNumber(pageNumber - 2)
+  }
+  const increasePageNumber = () => {
+    if (pageNumber < maxPage)
+      setPageNumber(pageNumber + 2)
+  }
+
   return (
     <section className={table}>
       <div className={bookContainer}>
-      <button className={button} onClick={() => {
-        if (pageNumber > minPage)
-          setPageNumber(pageNumber - 2)
-        }}>Previous</button>
         <div className={sleeve}>
+          <div className={leftPageTrigger} onClick={decreasePageNumber}>Left</div>
           {pageNumber === 1 ? <div className={page}>Page 1</div> : null}
           {pageNumber === 1 ? <div className={page}>Page 2</div> : null}
           {pageNumber === 3 ? <div className={page}>Page 3</div> : null}
+          {pageNumber === 3 ? <div className={page}>Page 4</div> : null}
+          <div className={rightPageTrigger} onClick={increasePageNumber}>Right</div>
         </div>
-            <button className={button} onClick={() => {
-        if (pageNumber < maxPage)
-          setPageNumber(pageNumber + 2)
-        }}>Next</button>
     </div>
     </section>
   )
