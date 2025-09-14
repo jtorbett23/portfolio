a554d32a3db8d804265e5d4b89964b6f215c0c35
jtorbett23 <josh.torbett@outlook.com>
2025-09-14 23:33:52 +0100

    use local storage in effect

--- src/components/theme-selector.tsx
+++ src/components/theme-selector.tsx
@@ -10,7 +10,7 @@ const ThemeSelector = ({defaultTheme, handleChange}: ThemeSelectorProps) => {
   return (
     <>
     <label className={themeLabel}>Theme:</label>
-        <select name="theme" id="theme" defaultValue={defaultTheme} onChange={handleChange}>
+        <select name="theme" id="theme" defaultValue={defaultTheme} value={defaultTheme} onChange={handleChange}>
         {/* <option value="book">Book</option> */}
         <option value="light">Light</option>
         <option value="dark">Dark</option>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Standard from "../components/standard/standard";
 import Book from "../components/book/book";
 import ThemeSelector from "../components/theme-selector";
 import {index, header} from "../styles/index.module.css"
-import useLocalStorage from "use-local-storage";
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [theme, setTheme] = useLocalStorage("theme", "light");
-  console.log(theme)
+  const [theme, setTheme] = useState("light")
+
+  useEffect(() => {
+    if(localStorage.getItem("theme") === null)
+      localStorage.setItem("theme", "light")
+    else
+      setTheme(String(localStorage.getItem("theme")))
+  },[typeof window !== "undefined" && typeof window.localStorage !== undefined])
   return (
     <div className={index} data-theme={theme}>
       <header className={header}>
         <ThemeSelector defaultTheme={theme} handleChange={
-          (e: React.ChangeEvent<HTMLInputElement>) => setTheme(e.target.value)} />
+          (e: React.ChangeEvent<HTMLInputElement>) => {
+            localStorage.setItem("theme", e.target.value)
+            setTheme(e.target.value)}} />
       </header>
       {theme.includes("book") ? <Book/> : <Standard/>}
     </div>
