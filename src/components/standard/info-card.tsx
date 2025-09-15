2ebc2069f796eadd8f7c6f1ca1bf7e38a3f58217
jtorbett23 <josh.torbett@outlook.com>
2025-09-15 18:57:22 +0100

    add key to map

--- src/components/standard/info-card.tsx
+++ src/components/standard/info-card.tsx
@@ -11,9 +11,8 @@ type InfoCardProps = {
 const InfoCard = ({infos} : InfoCardProps ) => {
   return (
 	<article className={`${card} ${centered}`}>
-		{/* <h2 className={cardHeading}>About Me</h2> */}
-		{infos.map((info : Contact)=> {
-			return <div className={infoText}><span className={centered}>{getIcon(info.title)} {info.title}:</span> <span>{info.content}</span></div>
+		{infos.map((info : Contact, index:number)=> {
+			return <div key={index} className={infoText}><span className={centered}>{getIcon(info.title)} {info.title}:</span> <span>{info.content}</span></div>
 		})}
 		
 	</article>
